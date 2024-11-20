527420f6263ec84e2c1b6ebc9701a884d325647b
beprathap <prathapboddumail@gmail.com>
2024-11-20 11:19:44 -0600

    Test

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 API_KEY = os.getenv('YOUTUBE_API_KEY')
-CHANNEL_IDS = ['UCX6OQ3DkcsbYNE6H8uQQuVA', #MrBeast
+CHANNEL_IDS = ['UCX6OQ3DkcsbYNE6H8uQQuVA', #MrBeastt
               'UCq-Fj5jknLsUf-MWSy4_brA', #T-Series
               'UCbCmjCuTUZos6Inko4u57UQ', #Cocomelon-Nursery Rhymes
               'UCpEhnqL0y41EpW2TvWAHD7Q' #SET India
