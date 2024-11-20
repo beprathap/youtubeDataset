7b1215c246e50df8456efbcdb4fb5a681c9ded84
beprathap <prathapboddumail@gmail.com>
2024-11-20 11:50:20 -0600

    add new channels

--- src/config.py
+++ src/config.py
@@ -7,10 +7,17 @@ load_dotenv()
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 API_KEY = os.getenv('YOUTUBE_API_KEY')
-CHANNEL_IDS = ['UCX6OQ3DkcsbYNE6H8uQQuVA', #MrBeastt
-              'UCq-Fj5jknLsUf-MWSy4_brA', #T-Series
-              'UCbCmjCuTUZos6Inko4u57UQ', #Cocomelon-Nursery Rhymes
-              'UCpEhnqL0y41EpW2TvWAHD7Q' #SET India
-             ]
+CHANNEL_IDS = ['UClFSU9_bUb4Rc6OYfTt5SPw', # Ask Me Anything: Philip DeFranco Show
+                'UCX6OQ3DkcsbYNE6H8uQQuVA', # Challenge: MrBeast
+                'UCsooa4yRKGN_zEE8iknghZA', # Educational: Ted-Ed
+                'UCh9IfI45mmk59eDvSWtuuhQ', # Funny Videos/Comedy: Ryan George
+                'UCBJycsmduvYEL83R_U4JriQ', # Product Demo/Launch: MKBHD
+                'UC4rlAVgAK0SGk-yTfe48Qpw', # Knowledge: BRIGHT SIDE
+                'UC0v-tlzsn0QZwJnkiaUSJVQ', # Reaction Videos: React
+                'UCzQUP1qoWDoEbmsQxvdjxgQ', # Video Podcasts: PowerfulJRE
+                'UCoYATiiqNT7csPFqtWWfc5w', # Video Tutorials: Skillshare
+                'UC-lHJZR3Gqxm24_Vd_AJ5Yw', # Gaming: PewDiePie
+                'UCq-Fj5jknLsUf-MWSy4_brA' # Music: T-Series
+               ]
 RAW_DATA_PATH = os.path.join(PROJECT_ROOT, 'data', 'raw')
 PROCESSED_DATA_PATH = os.path.join(PROJECT_ROOT, 'data', 'processed')
