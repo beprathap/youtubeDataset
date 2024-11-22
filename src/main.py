090a7136f730671c721a196caf1ac17bb4401847
beprathap <prathapboddumail@gmail.com>
2024-11-21 21:03:46 -0600

    Revert "add new features"
    This reverts commit 1d77cc2c9d536537f5f1b6152a2667ce625d6d3e.

--- src/main.py
+++ src/main.py
@@ -1,89 +1,34 @@
 import os # For file and path operations
 from datetime import datetime
-from src.config import API_KEY, CHANNEL_IDS, RAW_DATA_PATH
+from src.config import API_KEY, CHANNEL_IDS, RAW_DATA_PATH, PROCESSED_DATA_PATH
 from src.youtube_api import YouTubeAPI
+from src.data_processor import extract_required_fields
 
 def main():
     # Initialize YouTube API
-    # Initialize YouTube API with OAuth 2.0 credentials
-    api = YouTubeAPI(API_KEY,'config/credentials.json')
+    api = YouTubeAPI(API_KEY)
 
     # Get current timestamp
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
     # Extract and save raw JSON
-    # channel_stats = api.get_channel_stats(CHANNEL_IDS)
+    raw_data = api.get_channel_stats(CHANNEL_IDS)
+    if raw_data:
+        raw_filename = os.path.join(RAW_DATA_PATH, f'youtube_raw_{timestamp}.json')
+        api.save_raw_json(raw_data, raw_filename)
+        print(f"Raw data saved to {raw_filename}")
 
-    # Extract and save channel stats
-    # if channel_stats:
-    #     raw_filename = os.path.join(RAW_DATA_PATH, f'channel_stats_{timestamp}.json')
-    #     api.save_raw_json(channel_stats, raw_filename)
-    #     print(f"Raw data saved to {raw_filename}")
-    # else:
-    #     print("Failed to retrieve data")
+        # Print raw_data structure for debugging
+        print("Raw data structure:", type(raw_data))
+        if isinstance(raw_data, dict):
+            print("Keys in raw_data:", raw_data.keys())
+        elif isinstance(raw_data, list):
+            print("Number of items in raw_data:", len(raw_data))
 
-    # Extract and save video details for each channel
-    all_video_details = []
-    video_ids = [] # List to store all video IDs
-    for channel_id in CHANNEL_IDS:
-        latest_videos = api.get_latest_videos(channel_id)
-        if latest_videos:
-            print(f"Retrieved video details for channel {channel_id}")
-            all_video_details.extend(latest_videos) # Add videos to the master list
-
-            # Extract video IDs and add them to the video_ids list
-            video_ids.extend([video['id'] for video in latest_videos])
-
-    # Save all video details to a single JSON file
-    if all_video_details:
-        raw_filename = os.path.join(RAW_DATA_PATH, f'video_details_{timestamp}.json')
-        api.save_raw_json(all_video_details, raw_filename)
-
-    # Extract and save video comments
-    # all_comments = []
-    # for video in all_video_details:
-    #     video_id = video['id']
-    #     comments = api.get_video_comments(video_id)
-    #     if comments:
-    #         print(f"Retrieved comments for video {video_id}")
-    #         for comment in comments:
-    #             comment_data = {
-    #                 'video_id': video_id,
-    #                 'comment_id': comment['id'],
-    #                 'author_name': comment['snippet']['topLevelComment']['snippet']['authorDisplayName'],
-    #                 'text': comment['snippet']['topLevelComment']['snippet']['textDisplay'],
-    #                 'like_count': comment['snippet']['topLevelComment']['snippet']['likeCount'],
-    #                 'published_at': comment['snippet']['topLevelComment']['snippet']['publishedAt']
-    #             }
-    #             all_comments.append(comment_data)
-    #     else:
-    #         print(f"Failed to retrieve comments for video {video_id}")
-    #
-    # # Save all comments to a single JSON file
-    # if all_comments:
-    #     comment_filename = os.path.join(RAW_DATA_PATH, f'video_comments_{timestamp}.json')
-    #     api.save_raw_json(all_comments, comment_filename)
-    #     print(f"Video comments saved to {comment_filename}")
-    # else:
-    #     print("No comments were retrieved")
-
-    # Extract and save video/channel stats
-    daily_stats = api.get_daily_stats(video_ids)
-    if daily_stats:
-        daily_stats_filename = os.path.join(RAW_DATA_PATH, f'daily_stats_{timestamp}.json')
-        api.save_raw_json(daily_stats, daily_stats_filename)
-        print(f"Video categories saved to {daily_stats_filename}")
+        # Process data
+        extracted_data = extract_required_fields(raw_data)
     else:
-        print("Failed to retrieve daily statistics")
-
-    # Extract and save video categories
-    # video_categories = api.get_video_categories(video_ids)
-    # if video_categories:
-    #     categories_filename = os.path.join(RAW_DATA_PATH, f'video_categories_{timestamp}.json')
-    #     api.save_raw_json(video_categories, categories_filename)
-    #     print(f"Video categories saved to {categories_filename}")
-    # else:
-    #     print("Failed to retrieve video categories")
+        print("Failed to retrieve data")
 
 if __name__ == "__main__":
     main()
--- src/youtube_api.py
+++ src/youtube_api.py
@@ -1,43 +1,11 @@
-import os
 import json
-from datetime import datetime, timedelta
-
-import googleapiclient
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-
 class YouTubeAPI:
-    SCOPES = ['https://www.googleapis.com/auth/youtube.readonly',
-              'https://www.googleapis.com/auth/yt-analytics.readonly']
-
-    def __init__(self, api_key, credentials_file):
+    def __init__(self, api_key):
         # Builds a new instance of YouTube.
         self.youtube = build('youtube', 'v3', developerKey=api_key)
-        self.youtube_analytics = None
-        self.authenticate(credentials_file)
-
-    def authenticate(self, credentials_file):
-        # Define the scopes for YouTube Data API and YouTube Analytics API
-        SCOPES = [
-            "https://www.googleapis.com/auth/youtube.readonly",
-            "https://www.googleapis.com/auth/yt-analytics.readonly"
-        ]
-
-        # Check if token.json exists for storing access tokens
-        if os.path.exists('config/token.json'):
-            creds = Credentials.from_authorized_user_file('config/token.json', self.SCOPES)
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(credentials_file, self.SCOPES)
-            creds = flow.run_local_server(port=0)
-            # Save the credentials for future use
-            with open('config/token.json', 'w') as token:
-                token.write(creds.to_json())
-
-        # Build the YouTube and YouTube Analytics service objects
-        self.youtube_analytics = build('youtubeAnalytics', 'v2', credentials=creds)
 
     def get_channel_stats(self, channel_ids):
         try:
@@ -51,120 +19,8 @@ class YouTubeAPI:
             print(f'An error occured: {e}')
             return None
 
-    def get_latest_videos(self, channel_id, max_results=10):
-        try:
-            request = self.youtube.search().list(
-                part='id, snippet',
-                channelId=channel_id,
-                type='video',
-                order='date',
-                maxResults=max_results
-            )
-            response = request.execute()
-            video_ids = [item['id']['videoId'] for item in response['items']]
-            return self.get_video_details(video_ids)
-        except HttpError as e:
-            print(f'An error occurred: {e}')
-            return None
-
-    def get_video_details(self, video_ids):
-        try:
-            request = self.youtube.videos().list(
-                part='snippet, contentDetails, statistics',
-                id=','.join(video_ids)
-            )
-            response = request.execute()
-            return response['items']
-        except HttpError as e:
-            print(f'An error occurred: {e}')
-            return None
-
-
-    def get_video_comments(self, video_id, max_results=50):
-        try:
-            request = self.youtube.commentThreads().list(
-                part='snippet',
-                videoId=video_id,
-                maxResults=max_results
-            )
-            response = request.execute()
-            return response['items']
-        except HttpError as e:
-            print(f'An error occurred: {e}')
-            return None
-
-    def get_daily_stats(self, video_ids, days=28):
-        try:
-            end_date = datetime.now().date()
-            start_date = end_date - timedelta(days=days)
-
-            daily_stats = []
-
-            for video_id in video_ids:
-                # Get video details to retrieve channel_id
-                video_details = self.get_video_details([video_id])
-                if not video_details:
-                    continue  # Skip if no details found
-
-                channel_id = video_details[0]['snippet']['channelId']
-
-                # Query the YouTube Analytics API for daily stats
-                request = self.youtube_analytics.reports().query(
-                    ids=f'channel=={channel_id}',
-                    startDate=start_date.isoformat(),
-                    endDate=end_date.isoformat(),
-                    metrics='views,likes,dislikes,comments',
-                    dimensions='day',
-                    filters=f'video=={video_id}'
-                )
-                response = request.execute()
-
-                for row in response.get('rows', []):
-                    date, views, likes, dislikes, comments = row
-                    daily_stats.append({
-                        'date': date,
-                        'channel_id': channel_id,
-                        'video_id': video_id,
-                        'view_count': int(views),
-                        'like_count': int(likes),
-                        'dislike_count': int(dislikes),
-                        'comment_count': int(comments)
-                    })
-
-            return daily_stats
-        except HttpError as e:
-            print(f'An error occurred: {e}')
-            return None
-
-    def get_video_categories(self, video_ids):
-        try:
-            categories = {}
-            for video_id in video_ids:
-                request = self.youtube.videos().list(
-                    part='snippet',
-                    id=video_id
-                )
-                response = request.execute()
-                if 'items' in response and len(response['items']) > 0:
-                    category_id = response['items'][0]['snippet']['categoryId']
-                    category_request = self.youtube.videoCategories().list(
-                        part='snippet',
-                        id=category_id
-                    )
-                    category_response = category_request.execute()
-                    if 'items' in category_response and len(category_response['items']) > 0:
-                        category_title = category_response['items'][0]['snippet']['title']
-                        categories[video_id] = {
-                            'category_id': int(category_id),
-                            'title': category_title
-                        }
-            return categories
-        except HttpError as e:
-            print(f'An error occurred: {e}')
-            return None
-
-    # Add more methods for other API calls (e.g., get_video_stats, get_comments)
     # This method saves the raw JSON data into a file
     def save_raw_json(self, data, filename):
         with open(filename, 'w') as f:
-            json.dump(data, f, indent=4)
+            json.dump(data, f)
+# Add more methods for other API calls (e.g., get_video_stats, get_comments)
